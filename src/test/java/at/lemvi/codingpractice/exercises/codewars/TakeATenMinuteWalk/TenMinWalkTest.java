2b3d8ef160fe860fb2c76bbab4f9badbab8dfe6e
Viktor Lemanski <viktor.lemanski@tuta.io>
2021-09-15 23:47:54 +0200

    codewars exercise: added another test case to TenMinWalkTest

--- src/test/java/at/lemvi/codingpractice/exercises/codewars/TakeATenMinuteWalk/TenMinWalkTest.java
+++ src/test/java/at/lemvi/codingpractice/exercises/codewars/TakeATenMinuteWalk/TenMinWalkTest.java
@@ -12,5 +12,6 @@ class TenMinWalkTest {
         assertEquals("Should return false", false, TenMinWalk.isValid(new char[] {'w','e','w','e','w','e','w','e','w','e','w','e'}));
         assertEquals("Should return false", false, TenMinWalk.isValid(new char[] {'w'}));
         assertEquals("Should return false", false, TenMinWalk.isValid(new char[] {'n','n','n','s','n','s','n','s','n','s'}));
+        assertEquals("Should return false", false, TenMinWalk.isValid(new char[] {'n','n','n','s','s','w','w','e','e','e'}));
     }
 }
