7ee7af9590290aa6e865b9fcee7dce7036e411b1
Viktor Lemanski <viktor.lemanski@tuta.io>
2021-09-22 11:36:54 +0200

    hackerrank 30 days of code: Loops

---
+++ src/main/java/at/lemvi/codingpractice/exercises/hackerrank/thirtydaysofcode/Loops/Solution.java
@@ -0,0 +1,23 @@
+package at.lemvi.codingpractice.exercises.hackerrank.thirtydaysofcode.Loops;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Link to exercise:
+https://www.hackerrank.com/challenges/30-loops/problem
+ */
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i = 1; i <= 10; i++) {
+            int result = n * i;
+            System.out.println(n + " x " + i + " = " + result);
+        }
+        bufferedReader.close();
+    }
+}
