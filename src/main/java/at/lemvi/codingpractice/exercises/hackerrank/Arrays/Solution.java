8224d7967c70a5ab41fba810e664f833da84f76e
Viktor Lemanski <viktor.lemanski@tuta.io>
2021-09-22 12:36:01 +0200

    hackerrank 30 days of code: Arrays

---
+++ src/main/java/at/lemvi/codingpractice/exercises/hackerrank/Arrays/Solution.java
@@ -0,0 +1,37 @@
+package at.lemvi.codingpractice.exercises.hackerrank.Arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/*
+Link to exercise:
+https://www.hackerrank.com/challenges/30-arrays/problem
+ */
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                  .map(Integer::parseInt)
+                                  .collect(toList());
+
+        Collections.reverse(arr);
+
+        System.out.println(arr.stream()
+                              .map(integer -> String.valueOf(integer))
+                              .reduce((integer, integer2) -> integer + " " + integer2)
+                              .orElse("error"));
+
+        bufferedReader.close();
+    }
+}
