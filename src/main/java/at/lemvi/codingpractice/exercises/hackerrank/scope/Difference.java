e2fa418fbaf7299594a9cbda042e100a7e041e07
Viktor Lemanski <viktor.lemanski@tuta.io>
2021-10-06 18:00:50 +0200

    hackerrank 30 days of code: Scope

---
+++ src/main/java/at/lemvi/codingpractice/exercises/hackerrank/scope/Difference.java
@@ -0,0 +1,30 @@
+package at.lemvi.codingpractice.exercises.hackerrank.scope;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+Link to exercise:
+https://www.hackerrank.com/challenges/30-scope
+ */
+
+public class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    public Difference(int[] elements) {
+        this.elements = elements;
+    }
+
+    public void computeDifference() {
+        List<Integer> intArrList = Arrays.stream(elements).mapToObj(value -> value).collect(Collectors.toList());
+        int min = Collections.min(intArrList);
+        int max = Collections.max(intArrList);
+        int result = Math.abs(max) - Math.abs(min);
+        this.maximumDifference = result;
+    }
+
+}
